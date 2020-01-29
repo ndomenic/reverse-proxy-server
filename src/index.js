c331a9421b2544ecdd136896f19983a23b92d07c
Nick <ndomenic@uoguelph.ca>
2020-01-29 15:05:32 -0500

    Added https

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
+const https = require('https')
+const fs = require('fs')
 const { routes } = require('./routesConfig.json');
 
 const app = express();
 
 //Re-route each request to the appropriate route
 for (route of routes) {
-    app.use(route.route,
-        proxy({
-            target: route.address,
-            changeOrigin: true,
-        })
-    );
+  app.use(route.route,
+    proxy({
+      target: route.address,
+      changeOrigin: true,
+    })
+  );
 }
 
 //Listen for requests on the specified port 
-app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app).listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
