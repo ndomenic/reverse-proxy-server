fab833d784bb74a8dba0dc5dbd167bf91ecbb9a1
Nicholas Domenichini <ndomenic@uoguelph.ca>
2020-01-30 00:32:44 -0500

    Trying more stuff for https

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
-const https = require('https')
-const fs = require('fs')
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const { routes } = require('./routesConfig.json');
 
 const app = express();
+
+//Cert
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
 
 //Re-route each request to the appropriate route
 for (route of routes) {
@@ -16,8 +22,14 @@ for (route of routes) {
   );
 }
 
-//Listen for requests on the specified port 
+//Listen for http requests on the specified port 
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+//Listen for https requests on the specified port 
 https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app).listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+}, app).listen(443, () => console.log(`Listening on port 443`));
