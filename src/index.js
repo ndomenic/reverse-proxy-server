01ea0325d5552b4789d15f1e9ea3fde5b8abfb05
Nick Domenichini <nicholician@gmail.com>
2020-01-30 16:42:11 -0500

    Merge https into master (#1)
    * Created https branch
    
    * Updated gitignore
    
    * Completed https sertup

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const proxy = require('http-proxy-middleware');
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
-const { routes } = require('./routesConfig.json');
+let routes;
+if (process.env.NODE_ENV == 'production') {
+  routes = require('./routesConfigProduction.json').routes;
+} else {
+  routes = require('./routesConfigDevelopment.json').routes;
+}
 
 const app = express();
-
-//Cert
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
 
 //Re-route each request to the appropriate route
 for (route of routes) {
@@ -22,14 +22,21 @@ for (route of routes) {
   );
 }
 
-//Listen for http requests on the specified port 
-httpServer.listen(80, () => {
-	console.log('HTTP Server running on port 80');
-});
-
 //Listen for https requests on the specified port 
-https.createServer({
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-}, app).listen(443, () => console.log(`Listening on port 443`));
+if (process.env.NODE_ENV == 'production') {
+  //Certificate
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/domenichini.ca/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/domenichini.ca/cert.pem', 'utf8');
+  const ca = fs.readFileSync('/etc/letsencrypt/live/domenichini.ca/chain.pem', 'utf8');
+
+  https.createServer({
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  }, app).listen(443, () => console.log(`Listening on port 443`));
+}
+
+//Default http server
+http.createServer(app).listen(80, () => {
+  console.log('HTTP Server running on port 80');
+});
